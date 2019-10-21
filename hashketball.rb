3373cbe6a95e311b6b8d3cc0e2315b1fe60b202d
Kyle Cole <kylepcole@gmail.com>
2019-10-21 16:27:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -235,7 +235,7 @@ def big_shoe_rebounds
       end #=> END if attribute == :players
     
     end #=> END team.each
-    puts shoe_sizes.sort_by |k, v| v
+    puts shoe_sizes.sort_by {|k, v| v}
     
   end #=> END game_hash.each
   
