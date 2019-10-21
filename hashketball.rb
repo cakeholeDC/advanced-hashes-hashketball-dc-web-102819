3ae5fa033a282571aa60f0ed20caf2199b8e16b8
Kyle Cole <kylepcole@gmail.com>
2019-10-21 16:10:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_numbers(team_name)
             puts player
             puts stats[:number]
             numbers.push(stats[:number])
-            pp numbers
+            pp numbers.sort
           end #=> END [:players] => data.each
         end #=> END if attribute == :players
       end #=> END team.each
